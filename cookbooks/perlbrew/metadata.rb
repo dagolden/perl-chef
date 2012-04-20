f2580afd9fbe41f11277c2e853012242ad8fd66e
David Golden <dagolden@cpan.org>
2012-04-20 15:34:38 -0400

    reorder perlbrew metadata lines

--- cookbooks/perlbrew/metadata.rb
+++ cookbooks/perlbrew/metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "David Golden"
 maintainer_email "dagolden@cpan.org"
 license          "Apache 2.0"
+version          "0.1.0"
 description      "Installs/Configures perlbrew"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
 recipe           "perlbrew::default", "Installs/updates perlbrew"
 supports         "debian"
 supports         "ubuntu"
