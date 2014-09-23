b6a3a216565810818a64d101ea7bd0b6f48a0c85
David Golden <dagolden@cpan.org>
2014-09-23 11:50:45 -0400

    bump cookbook versions to clarify they are newer than previous release

--- cookbooks/carton/metadata.rb
+++ cookbooks/carton/metadata.rb
@@ -1,12 +1,11 @@
 maintainer       "David Golden"
 maintainer_email "dagolden@cpan.org"
 license          "Apache 2.0"
-version          "0.1.0"
+version          "0.1.1"
 description      "Installs/Configures carton and provides a LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
 recipe           "carton::default", "Installs carton LWRP dependencies"
 supports         "debian"
 supports         "ubuntu"
-depends          "perlbrew", ">= 0.1.0"
+depends          "perlbrew", ">= 0.1.1"
 depends          "runit", ">= 0.15.0"  # this is a lie, see README.md
--- cookbooks/perlbrew/metadata.rb
+++ cookbooks/perlbrew/metadata.rb
@@ -2,7 +2,7 @@ name             "perlbrew"
 maintainer       "David Golden"
 maintainer_email "dagolden@cpan.org"
 license          "Apache 2.0"
-version          "0.1.0"
+version          "0.1.1"
 description      "Installs/Configures perlbrew"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 recipe           "perlbrew::default", "Installs/updates perlbrew"
